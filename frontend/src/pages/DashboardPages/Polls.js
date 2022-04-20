576cae31adca143dd066286bbf344e69c1aa4399
IDOKO EMMANUEL <pidox31102002@gmail.com>
2022-04-20 23:30:46 +0100

    create poll page and polls page update

--- frontend/src/pages/DashboardPages/Polls.js
+++ frontend/src/pages/DashboardPages/Polls.js
@@ -1,20 +1,55 @@
-
-
 const Polls = (props) => {
+  const polls = [
+    { name: "Head Boy" },
+    { name: "Head Of Department Science" },
+    { name: "Vice Principal Academics" },
+  ];
+
+  const navigation = [
+    { name: "All Polls", href: "/", current: true },
+    { name: "My Polls", href: "/dashboard/create-poll", current: false },
+
+  ];
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
   return (
     <>
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">Create A Poll</h1>
+          <h1 className="text-3xl font-bold text-gray-900">Poll History</h1>
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <div className="hidden md:block">
+                      <div className="ml-10 flex items-baseline space-x-4">
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? "bg-gray-900 text-white"
+                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                              "px-3 py-2 rounded-md text-sm font-medium"
+                            )}
+                            aria-current={item.current ? "page" : undefined}
+                          >
+                            {item.name}
+                          </a>
+                         ))} 
+                      </div>
+                    </div>
+          </div>
         </div>
       </header>
+
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           {/* Replace with your content */}
-          <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-          </div>
-          {/* /End replace */}
+          {polls.map((poll) => {
+            <></>;
+          })}
         </div>
       </main>
     </>
