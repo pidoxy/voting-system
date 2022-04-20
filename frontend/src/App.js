be2116e482d0ea90c5a1c3ffc188d2cd68c88eb4
IDOKO EMMANUEL <pidox31102002@gmail.com>
2022-04-20 10:19:38 +0100

    setup wallet connect and redirect if metamask is not installed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,43 @@
-
+import { useState } from "react";
+import { ThemeProvider } from "./components/ThemeContext";
+import LandingPage from "./pages/LandinPage";
+import MetaMaskOnboarding from "@metamask/onboarding";
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState();
+
+  const currentUrl = new URL(window.location.href);
+  const forwarderOrigin =
+    currentUrl.hostname === "localhost" ? "http://localhost:9010" : undefined;
+
+  const { isMetaMaskInstalled } = MetaMaskOnboarding;
+
+  const connectWallet = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      console.log("MetaMask is installed!");
+    } else {
+      let onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+      onboarding.startOnboarding();
+    }
+
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      if (addressArray.length > 0) {
+        setCurrentAccount(addressArray[0]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      
-    </div>
+    <ThemeProvider>
+      <div className="">
+        <LandingPage connectWallet={connectWallet} />
+      </div>
+    </ThemeProvider>
   );
 }
 
