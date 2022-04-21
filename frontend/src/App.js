a57e031707220acd8fe4b2cc77e8a61a91a6cbc1
IDOKO EMMANUEL <pidox31102002@gmail.com>
2022-04-21 01:50:08 +0100

    omooo I've tried lemme rest small- contract functions refuse to be functions || connected Ui with smart contract

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,39 +4,34 @@ import LandingPage from "./pages/LandingPage";
 import MetaMaskOnboarding from "@metamask/onboarding";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import { ethers } from "ethers";
+import CONSTANTS from "./utils/constants";
+import { useEffect } from "react";
 
 function App() {
   // Destructured Ethereum window object
-  const { ethereum } = window;
+  const { ethereum, web3 } = window;
 
   // Account Details
   const [currentAccount, setCurrentAccount] = useState("");
   const [chainId, setChainId] = useState("");
   const [networkId, setNetworkId] = useState("");
+  const [contract, setContract] = useState("");
 
-  
   const currentUrl = new URL(window.location.href);
   const forwarderOrigin =
     currentUrl.hostname === "localhost" ? "http://localhost:9010" : undefined;
 
-    // connect wallet function
+  // connect wallet function
   const connectWallet = async () => {
     if (typeof window.ethereum !== "undefined") {
       console.log("MetaMask is installed!");
-      const addressArray = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-
-      if (addressArray.length > 0) {
-        setCurrentAccount(addressArray);
-      }
     } else {
       let onboarding = new MetaMaskOnboarding({ forwarderOrigin });
       onboarding.startOnboarding();
     }
 
     // alert("MetaMask is installed!");
-
   };
 
   // get connected wallet account details
@@ -45,21 +40,59 @@ function App() {
       const addressArray = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
-
-      if (addressArray.length > 0) {
-        setCurrentAccount(addressArray[0]);
-      }
-
       const chain = await ethereum.request({
         method: "eth_chainId",
       });
       const network = await ethereum.request({
         method: "net_version",
       });
-      setChainId(chain);
-      setNetworkId(network);
+
+      if (addressArray.length > 0) {
+        setCurrentAccount(addressArray[0]);
+        setChainId(chain);
+        setNetworkId(network);
+      }
     }
   })();
+
+  const callFunc = () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const address = signer.getAddress();
+      const contract = new ethers.Contract(
+        CONSTANTS.CONTRACT_ADDRESS_ACCESSCONTROL,
+        CONSTANTS.ACCESSCONTROL_ABI,
+        signer
+      );
+      console.log(contract);
+      console.log(address);
+      setContract(contract);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // check role of user that logged in
+  const checkRole = async (currentAccount) => {
+    try {
+      // loading alert to show transaction is being sent
+      console.log("Loading...");
+      const tx = await contract;
+      tx.addRole()
+      tx.wait();
+
+      // alert success message
+      console.log("Transaction Successful!");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    callFunc();
+    checkRole();
+  }, []);
 
   return (
     <ThemeProvider>
@@ -73,12 +106,19 @@ function App() {
                 currentAccount={currentAccount}
                 networkId={networkId}
                 connectWallet={connectWallet}
+                chainId={chainId}
               />
             }
           />
           <Route
             path="/dashboard/*"
-            element={<Dashboard currentAccount={currentAccount} />}
+            element={
+              <Dashboard
+                ethers={ethers}
+                contract={contract}
+                currentAccount={currentAccount}
+              />
+            }
           />
           <Route
             path="/*"
